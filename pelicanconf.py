743563036d218eabe562c51c1e39388272acea34
Sam Itskin <sitskin@gmail.com>
2017-02-12 20:09:49 -0600

    Deploying changes

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
+from datetime import date
 
 AUTHOR = 'Sam Itskin'
 SITENAME = 'The Itskin Blog'
 SITESUBTITLE = 'Here I blog about stuff that I find interresting'
 SITEURL = 'https://sitskin.github.io'
+SITE_DESCRIPTION = 'I\'m a recent graduate of Baylor University in Computer Science.  Although I\'ve just graduated, I\'m passionate about my field.  Living in Austin, Tx means that I\'m in close proximity to a plethera of established and up-and-coming software companies.'
+YEAR = date.today().year
+EMAIL = sitskin@gmail.com
 
 PATH = 'content'
 
@@ -33,11 +37,33 @@ SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
+PAGINATED_DIRECT_TEMPLATES = ('categories', 'archives')
+
 STATIC_PATHS = ['images',]
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
 TYPOGRAPHY = True
-THEME = "..\pelican-themes\octopress"
+THEME = "..\pelican-themes\backdrop"
+
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = [
+        'sitemap',
+        'representative_image'
+]
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'weekly',
+        'pages': 'monthly'
+    }
+}
 
 MARKUP = ('rst', 'md', )
